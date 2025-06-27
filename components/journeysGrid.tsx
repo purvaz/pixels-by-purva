00b35121631de0d2eb834a8ed70e5b31eeec75fa
purvaz <pzinjarde@sfsu.edu>
2025-06-27 16:26:49 -0700

    Dummy change to trigger redeploy

--- components/journeysGrid.tsx
+++ components/journeysGrid.tsx
@@ -1,4 +1,4 @@
-// Wrapper for the Journeys Page images
+// Wrapper for the Journeys Page images. 
 
 "use client";
 
@@ -75,3 +75,5 @@ export default function JourneysGrid({
     </>
   );
 }
+
+// Dummy change to trigger redeploy
