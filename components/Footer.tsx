b2d1a661acd5b9c003aa4756b817f8c925185fd0
purvaz <pzinjarde@sfsu.edu>
2025-07-15 16:59:29 -0700

    logo changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,18 +9,18 @@ export default function Footer() {
 
     return (
         <footer className="mt-auto border-t border-black px-4 md:px-8 lg:px-10 py-4 text-sm tracking-wide text-gray-700">
-            <div className="flex flex-col md:flex-row items-center justify-between gap-4">
+            <div className="flex flex-col md:flex-row items-center justify-between gap-6 pb-4">
                 {/* Left: Logo + Copyright */}
                 <div className="flex items-center gap-3 w-full md:w-auto justify-center md:justify-start">
                     <Image
-                        src="/logo/logo.png"
+                        src="/logo/purva.png"
                         alt="Purva Zinjarde Logo"
-                        width={70}
+                        width={120}
                         height={70}
                         className="object-contain"
                     />
                     <p className="text-sm text-gray-700 text-center md:text-left">
-                        © Copyright of Purva Zinjarde. All rights reserved.
+                        © Copyright of Purva Zinjarde-Joshi. All rights reserved.
                     </p>
                 </div>
 
